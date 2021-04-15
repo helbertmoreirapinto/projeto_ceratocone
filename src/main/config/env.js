4069a1037779ae00b6b80c399b2aad645b5874f6
Helbert Pinto <helbert.tec.info@gmail.com>
2021-04-14 23:07:50 -0300

    alterando database

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,9 @@
-module.exports = {
+const localhost = {
   MONGO_URL: 'mongodb://localhost:27017/ceratocone'
 }
+
+const dev = {
+  MONGO_URL: 'mongodb+srv://userApp:yeIGJEABqOFffY1J@apicluster.owqsv.mongodb.net/certaocone'
+}
+
+module.exports = dev
